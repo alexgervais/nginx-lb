cdbf3bc63764f68bb4696f2c558541b8281a3362
Alex Gervais <alex.gervais@gmail.com>
2013-01-29 19:08:52 -0500

    making sure the backports repo is added before performing apt-get update

--- puppet/modules/nginx/manifests/install.pp
+++ puppet/modules/nginx/manifests/install.pp
@@ -1,13 +1,14 @@
 class nginx::install {
  
-  common::line { 'debian_package':
+  common::line { 'debian_backports':
     file => '/etc/apt/sources.list',
     line => 'deb http://backports.debian.org/debian-backports squeeze-backports main';
   }
  
   exec { 'apt-update':
     command     => '/usr/bin/apt-get update --fix-missing',
-    refreshonly => false;
+    refreshonly => false,
+    require => Common::Line['debian_backports'];
   }
  
   package { 'nginx/squeeze-backports':
